16febd6c63ac429e46c949757922cf817a018843
Chickensoup269 <thientran01345@icloud.com>
2024-10-05 22:09:54 +0700

    V4 Update code

--- Frontend/src/components/layouts/DefaultLayout/Header/index.js
+++ Frontend/src/components/layouts/DefaultLayout/Header/index.js
@@ -23,15 +23,15 @@ export default function Header() {
   return (
     <header
       className={cx(
-        'wrapper-header bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600'
+        'wrapper-header fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600'
       )}
     >
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
+            src="https://logos-world.net/wp-content/uploads/2023/07/McDonalds-Logo.png"
+            className="h-10 rounded-sm"
+            alt="ZeroChiken Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             ZeroChicken
@@ -46,7 +46,7 @@ export default function Header() {
           />
           <button
             type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="btn-login focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 text-center "
           >
             <a href={routes.login}>Login</a>
           </button>
@@ -82,7 +82,7 @@ export default function Header() {
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border   md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  ">
             <li>
               <a
                 href="#"
