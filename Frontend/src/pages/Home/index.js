87d8a5851f4f1c11ce8f3f4b8822434f9ce0c9d9
Chickensoup269 <thientran01345@icloud.com>
2024-10-08 00:02:15 +0700

    V8 Update Code final

--- Frontend/src/pages/Home/index.js
+++ Frontend/src/pages/Home/index.js
@@ -193,6 +193,71 @@ export default function Home() {
           )
         )}
       </div>
+      <section class="py-24 relative">
+        <div class="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+          <div class="w-full justify-start items-center gap-12 grid lg:grid-cols-2 grid-cols-1">
+            <div class="w-full justify-center items-start gap-6 grid sm:grid-cols-2 grid-cols-1 lg:order-first order-last">
+              <div class="pt-24 lg:justify-center sm:justify-end justify-start items-start gap-2.5 flex">
+                <img
+                  class=" rounded-xl object-cover"
+                  src="https://i.pinimg.com/originals/24/50/fc/2450fcf22635b5cf4678aeaffb7be9c8.jpg"
+                  alt="about Us image"
+                />
+              </div>
+              <img
+                class="sm:ml-0 ml-auto rounded-xl object-cover"
+                src="https://i.pinimg.com/736x/c6/e4/5f/c6e45fb6b7a25a216754aff0abf82776.jpg"
+                alt="about Us image"
+              />
+            </div>
+            <div class="w-full flex-col justify-center lg:items-start items-center gap-10 inline-flex">
+              <div class="w-full flex-col justify-center items-start gap-8 flex">
+                <div class="w-full flex-col justify-start lg:items-start items-center gap-3 flex">
+                  <h2 class="about-kfc-bottom text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
+                    KFC Historical Archives
+                  </h2>
+                  <p class="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
+                    The Colonel is the most iconic symbol we have. We proudly
+                    display him as our symbol for all things Always Original,
+                    all the time.
+                  </p>
+                </div>
+                <div class="w-full lg:justify-start justify-center items-center sm:gap-10 gap-5 inline-flex">
+                  <div class="flex-col justify-start items-start inline-flex">
+                    <h3 class="about-kfc-bottom text-4xl font-bold font-manrope leading-normal">
+                      72+
+                    </h3>
+                    <h6 class="text-gray-500 text-base font-normal leading-relaxed">
+                      Years Of Service
+                    </h6>
+                  </div>
+                  <div class="flex-col justify-start items-start inline-flex">
+                    <h4 class="about-kfc-bottom text-4xl font-bold font-manrope leading-normal">
+                      140+
+                    </h4>
+                    <h6 class="text-gray-500 text-base font-normal leading-relaxed">
+                      Restaurant
+                    </h6>
+                  </div>
+                  <div class="flex-col justify-start items-start inline-flex">
+                    <h4 class="about-kfc-bottom text-4xl font-bold font-manrope leading-normal">
+                      52+
+                    </h4>
+                    <h6 class="text-gray-500 text-base font-normal leading-relaxed">
+                      Millions Of People
+                    </h6>
+                  </div>
+                </div>
+              </div>
+              <button class="about-kfc-bottom-btn sm:w-fit w-full px-3.5 py-2 transition-all duration-700 ease-in-out rounded-lg shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] justify-center items-center flex">
+                <span class="px-1.5 text-white text-sm font-medium leading-6">
+                  Read More
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
