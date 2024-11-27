124db9a3e89aea432a7737c4cb42b40da156279e
Zero_031 <95624468+ChickenSoup269@users.noreply.github.com>
2024-11-27 14:35:58 +0700

    Update index.js

--- Frontend/src/routes/index.js
+++ Frontend/src/routes/index.js
@@ -1,4 +1,4 @@
-import Home from '~/pages/home'
+import Home from '~/pages/Home'
 import Login from '~/pages/login'
 import Register from '~/pages/register'
 import RecoveryPassword from '~/pages/recovery-pass'
